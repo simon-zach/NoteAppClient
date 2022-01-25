f901039b11329390e3018aec19a02a60d2bfe594
simon-zach <zacharczukszymon@gmail.com>
2022-01-25 18:35:58 +0100

    refrectored

--- src/App.js
+++ src/App.js
@@ -1,21 +1,15 @@
 import React from "react";
 import Navigation from './Components/Navigation';
-import { Container } from "react-bootstrap";
-import { Routes, Route, Outlet, Link } from "react-router-dom";
-import { gql,useApolloClient,useQuery} from '@apollo/client'
-
-
-
+import {Container } from "react-bootstrap";
+import {Outlet } from "react-router-dom";
 
 function App() {
-
   return (
     <>
       <Navigation ></Navigation>
       <Container>
         <Outlet />
       </Container>
-      
     </>
   );
 }
